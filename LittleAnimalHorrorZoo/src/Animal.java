a2b57801c69f0758fdb3f9670da99c2a5e071bc3
mubibic <amal487d@stud.kea.dk>
2021-09-21 16:12:06 +0200

    name to type

--- LittleAnimalHorrorZoo/src/Animal.java
+++ LittleAnimalHorrorZoo/src/Animal.java
@@ -3,24 +3,24 @@ public class Animal {
     // instance fields or variables set private for encapsulation
     private String snake;
     private String rabbit;
-    private String name;
+    private String type;
     private int x;
     private  int y;
 
     // constructor method with arguments
-    public Animal(String name, int x, int y) {
+    public Animal(String type, int x, int y) {
         // assign parameter value to instance field
-        this.name = name;
+        this.type = type;
         this.x = x;
         this.y = y;}
 
     // getters and setters
-    public String getName() {
-        return name;
+    public String getType() {
+        return type;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setType(String type) {
+        this.type = type;
     }
 
     public int getX() {
@@ -41,7 +41,7 @@ public class Animal {
 
     // method for moving animal objects with one position at a time
     public void moveAnimal(){
-        if (getName().equals("Rabbit")) {
+        if (getType().equals("Rabbit")) {
             setX(getX()+1);
             setY(getY()+1);
         }
@@ -55,7 +55,7 @@ public class Animal {
     // creating custom toString method with 'override'-ing JAVA default method
     @Override
     public String toString() {
-        return "I am the " + name +
+        return "I am the " + type +
                 " and I'm now standing on square " + x + "," + y;
     }
 }
